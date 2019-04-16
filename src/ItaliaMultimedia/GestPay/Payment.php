0b9b2045cb876869b6b22993b78773a11effd332
Radu Maier <ariademur@webserv.co>
2019-04-16 16:14:19 +0200

    Payment.create

--- src/ItaliaMultimedia/GestPay/Payment.php
+++ src/ItaliaMultimedia/GestPay/Payment.php
@@ -1,7 +1,25 @@
 <?php
 namespace ItaliaMultimedia\GestPay;
 
+use WebServCo\Framework\Http\Method;
+
 final class Payment extends AbstractGestPay
 {
-    
+    public function create($amount, $shopTransactionId)
+    {
+        if (empty($this->shopLogin)) {
+            throw new \InvalidArgumentException('Missing shopLogin');
+        }
+        return $this->call(
+            'payment/create',
+            Method::POST,
+            [], // headers
+            [
+                'shopLogin' => $this->shopLogin,
+                'amount' => floatval($amount),
+                'currency' => $this->currency,
+                'shopTransactionID' => $shopTransactionId,
+            ]
+        );
+    }
 }
