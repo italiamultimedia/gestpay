691ffa91f654b8d6ff7a2111aa6d19dfaeecf3f7
Radu Maier <ariademur@webserv.co>
2019-04-16 15:27:13 +0200

    Url class

---
+++ src/ItaliaMultimedia/GestPay/Url.php
@@ -0,0 +1,8 @@
+<?php
+namespace ItaliaMultimedia\GestPay;
+
+final class Url
+{
+    const PRODUCTION = 'https://sandbox.gestpay.net/api/v1/';
+    const SANDBOX = 'https://ecomms2s.sella.it/api/v1/';
+}
