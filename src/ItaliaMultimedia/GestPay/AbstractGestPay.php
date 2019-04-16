35de533c8d2c530e1b641a3ca11ca5dd41e74c3e
Radu Maier <ariademur@webserv.co>
2019-04-16 12:32:19 +0200

    AbstractGestPay.setCurrency

--- src/ItaliaMultimedia/GestPay/AbstractGestPay.php
+++ src/ItaliaMultimedia/GestPay/AbstractGestPay.php
@@ -4,13 +4,20 @@ namespace ItaliaMultimedia\GestPay;
 abstract class AbstractGestPay
 {
     protected $apiKey;
+    protected $currency;
     protected $shopLogin;
 
     public function __construct($apiKey, $shopLogin)
     {
         $this->validate($apiKey, $shopLogin);
         $this->apiKey = $apiKey;
+        $this->currency = Currencies::EUR;
         $this->shopLogin = $shopLogin;
+    }
+
+    public function setCurrency($currency)
+    {
+        $this->currency = $currency;
     }
 
     protected function validate($apiKey, $shopLogin)
