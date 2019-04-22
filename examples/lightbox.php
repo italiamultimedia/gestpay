8ec0333e07cbf16d5cd532121c57baedad1b37d7
Radu Maier <ariademur@webserv.co>
2019-04-22 13:29:51 +0200

    cleanup

--- examples/lightbox.php
+++ examples/lightbox.php
@@ -16,12 +16,11 @@ $axerveScriptUrl = sprintf(
 $paymentID = isset($_GET['paymentID']) ? $_GET['paymentID'] : null;
 $paymentToken = isset($_GET['paymentToken']) ? $_GET['paymentToken'] : null;
 ?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>Gestpay</title>
     </head>
     <body>
